8e44b9418a799b86c16831b379eb23c28d5ccf4b
yashsdeshmukh <ydeshmukh0093@gmail.com>
2021-07-29 23:17:04 +0530

    First

--- src/main/java/com/dockerTestSpring/yashSample/resource/HelloWorldEndpoint.java
+++ src/main/java/com/dockerTestSpring/yashSample/resource/HelloWorldEndpoint.java
@@ -1,6 +1,7 @@
 package com.dockerTestSpring.yashSample.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/docker/hello")
 public class HelloWorldEndpoint {
 
-	@GetMapping
+	@PostMapping
 	public String helloWorld() {
 		return "Hello Yash and Warren and Svirtz and Everyone and hello WORLD !!!!";
 	}
+	@GetMapping
+	public String helloWorld2() {
+		return "Hello Yash and Warren and Svirtz and Everyone and hello WORLD !!!!";
+	}
 }
+
